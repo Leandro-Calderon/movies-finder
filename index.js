af75d2371c104e4e408ab78db61855029eb74910
Leandro-Calderon <mellyleo17@gmail.com>
2023-12-06 21:14:54 -0300

    ejercicio terminado

--- index.js
+++ index.js
@@ -1,28 +1,46 @@
-// con argvs capturar los input de consola
-// depurar el input con slice ?
-// const { argv } = require('node:process');
-const process = require("node:process");
+// En estas dos líneas, se importan los módulos process y pelis.js. El módulo process es un objeto global que proporciona información y control sobre el proceso actual de Node.js. El módulo pelis.js es un archivo que contiene las funciones que se utilizarán más adelante en el código.
+const process = require("process");
+const operations = require("./pelis.js");
 
-const { getAllMovies } = require("./pelis");
-// const peliculas = require('./pelis.json')
-const argumento = process.argv.slice(2);
+// Esta función parsingParameters() se encarga de analizar los argumentos de línea de comandos que se pasan al script. Primero, se utiliza process.argv.slice(2) para obtener los argumentos de línea de comandos a partir del tercer elemento (los primeros dos elementos son node y el nombre del archivo). Luego, se crea un objeto parameterObject que contiene dos propiedades: operation y parameters. La propiedad operation se establece en el segundo argumento de línea de comandos (si existe) después de eliminar los primeros dos caracteres (--). La propiedad parameters se establece en el tercer argumento de línea de comandos (si existe). Finalmente, la función devuelve el objeto parameterObject.
+function parsingParameters() {
+  const args = process.argv.slice(2);
+  const parameterObject = {
+    operation: args[0] ? args[0].slice(2) : null,
+    parameters: args[1],
+  };
+  return parameterObject;
+}
 
-// const input = process.argv.slice(2)
+// Esta función executOperation(operationObject) se encarga de ejecutar la operación solicitada por el usuario. Primero, se crea un objeto map que asigna cada operación a su función correspondiente en el archivo pelis.js. Luego, se extrae la operación y los parámetros del objeto operationObject. Si se proporciona una operación, se busca la función correspondiente en el objeto map y se llama con los parámetros proporcionados. Si se encuentra una función, se devuelve su resultado. Si no se encuentra una función, se muestra un mensaje de error y se devuelve null.
+function executOperation(operationObject) {
+  const map = {
+    sort: operations.sortMoviesBy,
+    search: operations.searchMoviesBy,
+    tag: operations.tagedMoviesBy,
+  };
+  const action = operationObject.operation;
+  const parameterToUse = operationObject.parameters;
 
-// aca va el diccionario para bindear la data con la accion
+  if (action) {
+    const executor = map[action](parameterToUse);
 
-// const commands = {
-//   '--': pelis.getAllMovies,
-//   '--sort': pelis.sortMoviesBy,
-//   '--search': pelis.searchMoviesBy,
-//   '--tag': pelis.tagedMoviesBy
-// }
-
-function main() {
-  if (argumento.length === 0) {
-    const resultado = getAllMovies();
-    console.table(resultado);
+    if (executor !== undefined) {
+      return executor;
+    } else {
+      return null;
+    }
   }
 }
-console.log(argumento);
+
+// Esta función main() es la función principal que se ejecuta cuando se inicia el script. Primero, se verifica si se proporcionaron argumentos de línea de comandos. Si es así, se analizan los argumentos y se ejecuta la operación correspondiente. Si no se proporcionan argumentos, se muestran todas las películas en una tabla.
+function main() {
+  if (process.argv[2]) {
+    const objectArgv = parsingParameters();
+    console.table(executOperation(objectArgv));
+  } else {
+    console.table(operations.getAllMovies());
+  }
+}
+
 main();
--- pelis.js
+++ pelis.js
@@ -1,52 +1,44 @@
-// traer el Json y parsearlo
 const fs = require("fs");
 const path = require("path");
 const data = fs.readFileSync(path.join(__dirname, "/pelis.json"));
-const peliculas = JSON.parse(data);
+const pelis = JSON.parse(data);
 
-// hacer el caseSensitive
-
-// ACA ABAJO VA CADA FUNCIÓN
-// -----------------------------------------------------------
-
-// mejorar la presentacion de getAllMovies con el Input "node index.js" es el trigger
+// FUNCIONES
 
 function getAllMovies() {
-  return peliculas;
+  return pelis;
 }
-// -----------------------------------------------------------
-
-// oredenar alfabeticamente "--sort title"
-// ordenar de menor a mayor en el rating "--sort rating"
-// node index.js --sort {nombreDeLaPropiedad}
 
 function sortMoviesBy(props) {
-  const resultado = peliculas.sort((a, b) => {
-    const valorA = a[props];
-    const valorB = b[props];
+  const result = pelis.sort((a, b) => {
+    const valueA = a[props];
+    const valueB = b[props];
 
-    if (typeof valorA === "string") {
-      return valorA.localeCompare(valorB);
+    if (typeof valueA === "string") {
+      return valueA.localeCompare(valueB);
     }
 
-    return valorB - valorA;
+    return valueA - valueB;
   });
-  return resultado;
+  return result;
 }
 
-// -----------------------------------------------------------
-// node index.js --search {searchCriteria}
-// El argumento --search debe devolver las películas que tienen el texto del argumento.
-// node index.js --search magic
-// Debería devolver las películas que tengan la palabra "magic" en su titulo.
-function searchMoviesBy() {}
-// -----------------------------------------------------------
-// Si le pasás el argumento --tag debe devolver las películas que tienen ese tag. "node index.js --tag {nombreDelTag}"
-// node index.js --tag thriller
-function tagedMoviesBy() {}
-// -----------------------------------------------------------
+function searchMoviesBy(word) {
+  const result = pelis.filter(
+    (movie) =>
+      movie.title.toLowerCase().includes(word.toLowerCase()) ||
+      movie.tags.some((tag) => tag.toLowerCase() === word.toLowerCase()),
+  );
+  return result;
+}
 
-// aca va el export de funciones
+function tagedMoviesBy(tag) {
+  const result = pelis.filter((movie) =>
+    movie.tags.some((t) => t.toLowerCase() === tag.toLowerCase()),
+  );
+  return result;
+}
+
 module.exports = {
   getAllMovies,
   sortMoviesBy,
